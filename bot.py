232720f20407953495cf4547c7af9ea3e1c1099a
Matin500-85 <2006matin500.85@gmail.com>
2025-11-24 16:13:19 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -116,18 +116,18 @@ def send_welcome(message):
     """پاسخ به دستور /start"""
     bot.reply_to(message, get_welcome_text1(), parse_mode='Markdown')
     time.sleep(0.5)
-    bot.reply_to(message, get_welcome_text2(), reply_markup=create_main_menu([btn for btn in all_buttons if !='start']))   
+    bot.reply_to(message, get_welcome_text2(), reply_markup=create_main_menu(['pay','help']))   
 
 
 @bot.message_handler(commands=['pay'])
 def send_pay(message):
     """پاسخ به دستور /pay"""
-    bot.reply_to(message, get_pay_text(), reply_markup=create_main_menu([btn for btn in all_buttons if !='pay']), parse_mode='Markdown')
+    bot.reply_to(message, get_pay_text(), reply_markup=create_main_menu(['start','help']), parse_mode='Markdown')
     
 @bot.message_handler(commands=['help'])
 def send_help(message):
     """پاسخ به دستور /help"""
-    bot.reply_to(message, get_help_text(), reply_markup=create_main_menu([btn for btn in all_buttons if !='help']), parse_mode='Markdown')
+    bot.reply_to(message, get_help_text(), reply_markup=create_main_menu(['start','pay']), parse_mode='Markdown')
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -141,12 +141,12 @@ def handle_inline_clicks(call):
         bot.send_message(
             call.message.chat.id, 
             get_welcome_text2(), 
-            reply_markup=create_main_menu([btn for btn in all_buttons if !='start'])
+            reply_markup=create_main_menu(['pay','help'])
         )
     elif call.data == 'show_pay':
-        bot.send_message(call.message.chat.id, get_pay_text(), reply_markup=create_main_menu([btn for btn in all_buttons if !='pay']), parse_mode='Markdown')
+        bot.send_message(call.message.chat.id, get_pay_text(), reply_markup=create_main_menu(['start','help']), parse_mode='Markdown')
     elif call.data == 'show_help':
-        bot.send_message(call.message.chat.id, get_help_text(), reply_markup=create_main_menu([btn for btn in all_buttons if !='help']), parse_mode='Markdown')
+        bot.send_message(call.message.chat.id, get_help_text(), reply_markup=create_main_menu(['start','pay']), parse_mode='Markdown')
     
     bot.answer_callback_query(call.id)
 
@@ -275,6 +275,7 @@ if __name__ == "__main__":
         bot.polling(none_stop=True, interval=2, timeout=30 , skip_pending=True)
     except Exception as e:
         logger.error(f"خطا در اجرای ربات: {e}")
+
 
 
 
