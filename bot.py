bfb32dd42ef878be041d4f621816e28fb6beb349
Matin500-85 <2006matin500.85@gmail.com>
2025-11-25 16:13:20 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 # توکن از متغیر محیطی می‌خونیم
-TOKEN = os.environ.get(BOT_TOKEN)
+TOKEN = os.environ.get('BOT_TOKEN')
 if not TOKEN:
     logger.error("❌ توکن ربات پیدا نشد! مطمئن شوی BOT_TOKEN تنظیم شده")
     exit(1)
@@ -293,6 +293,7 @@ if __name__ == "__main__":
     except Exception as error:
         logger.error(f"خطا در اجرای ربات: {error}")
         time.sleep(10)
+
 
 
 
